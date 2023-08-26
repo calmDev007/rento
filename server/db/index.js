5245abcd17a952c1976ac38616b844eb47aa5a61
My Name <om20082003@gmail.com>
2023-08-26 22:06:37 +0530

    database_schema_added

---
+++ server/db/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema ({
+    username: String,
+    email:String,
+    password: String,
+    profileimage:String,
+    purchased: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Property' }]
+})
+
+const User = mongoose.model('User', userSchema);
+
+const porpertySchema = new mongoose.Schema ({
+    type: String,
+    description: String,
+    price: Number,
+    imageLink: String,
+    location:String,
+    shortlink:String,
+    avialability: Boolean,
+    author: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+})
+
+const Property = mongoose.model('Admin', porpertySchema);
+console.log("corret");
+
+module.exports = {
+    User,
+    Property
+  }
---
+++ server/index.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://om:Ye6hkj4c3OvOtNYl@cluster0.ynonr4i.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "Rento-Db" });
