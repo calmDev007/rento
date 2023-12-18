67cab53d83abea2d8aee4a0e5aa318fa341518c9
Manish Singh <singh.manishh.224@gmail.com>
2023-12-18 23:28:24 +0530

    created a landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Landing } from './pages/landing'
+import { Landing } from './pages/Landing';
 import { TopBar } from './components/TopBar';
 import { Profile } from './pages/Profile';
 import { Views } from './pages/Views';
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,13 +1,26 @@
 import { Avilablepro } from "../components/Avilablepro"
 import { Button } from "@mui/material"
+import './Landing.css';
 import { Navigate, useNavigate } from "react-router-dom"
+import BannerBackground from "../assets/Group 1881.png";
 
 export const Landing = () => {
     const navigate = useNavigate();
     
-    return ( <>
+    return ( <div className="home-container">
         <Avilablepro/>
-        This is landing page
+        
+            <div className="landing-banner-container">
+                <div className="landing-banner-container">
+                    <img className="landingpage-img" src = {BannerBackground} alt="" />
+                </div>
+            
+                <div className="home-text-section">
+                    <h1 className="primary-heading">
+                    The most affortable place to <br />stay in the san franciso bay 
+                    </h1>
+                </div> 
+            </div>
 
-    </> )
+    </div> )
 }
